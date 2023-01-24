06bfc5de571cae4b53390d09838666eed91fc9e5
Lauri Alanen <laurialanen1@gmail.com>
2023-01-24 17:03:36 +0200

    Update blink.c

--- src/blink.c
+++ src/blink.c
@@ -7,17 +7,17 @@
 #define BLINK_LED GPIO_NUM_19
 
 int app_main(void){
-    char *ourTask = pcTaskGetName(NULL);
-    ESP_LOGI(ourTask, "Hello Starting up!\n");
+    char *ourTask = pcTaskGetName(NULL); // NULL Will make a query for it's own name 
+    ESP_LOGI(ourTask, "Hello Starting up!\n"); // Basically the same as Serial.print
 
 
-    gpio_reset_pin(BLINK_LED);
-    gpio_set_direction(BLINK_LED, GPIO_MODE_DEF_OUTPUT);
+    gpio_reset_pin(BLINK_LED); 
+    gpio_set_direction(BLINK_LED, GPIO_MODE_DEF_OUTPUT); // Set the BLINK_LED pin as an output.
 
     while (1)
     {
-        gpio_set_level(BLINK_LED, 1);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+        gpio_set_level(BLINK_LED, 1); // Set the BLINK_LED pin HIGH.
+        vTaskDelay(1000/portTICK_PERIOD_MS); // Calculate the delay time using the tick rate
         gpio_set_level(BLINK_LED, 0);
         vTaskDelay(1000/portTICK_PERIOD_MS);
     }
