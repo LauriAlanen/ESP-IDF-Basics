b7b8ff828766c36db8eb5f9b39a6a6180c502aad
Lauri Alanen <laurialanen1@gmail.com>
2023-01-24 11:18:30 +0200

    Testing fade-func

--- src/pwm.c
+++ src/pwm.c
@@ -8,9 +8,11 @@
 #define LEDC_OUTPUT_IO (19)
 #define LEDC_CHANNEL LEDC_CHANNEL_0
 #define LEDC_DUTY_RES LEDC_TIMER_13_BIT
-#define LEDC_DUTY (1200)
+#define LEDC_DUTY (200)
 #define LEDC_FREQUENCY (2000)
 
+#define LEDC_FADE_TIME 3000
+#define ESP_INT_FLAG_LEVEL (1<<1)
 
 static void init_ledc(void)
 {
@@ -42,7 +44,20 @@ static void init_ledc(void)
 
 int app_main(void){
     init_ledc();
-    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
-    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
+
+    ESP_ERROR_CHECK(ledc_set_fade_with_time(
+        LEDC_MODE,
+        LEDC_CHANNEL,
+        LEDC_DUTY,
+        LEDC_FADE_TIME
+    ));
+
+    ESP_ERROR_CHECK(ledc_fade_func_install(ESP_INT_FLAG_LEVEL));
+
+    ESP_ERROR_CHECK(ledc_fade_start(
+        LEDC_MODE,
+        LEDC_CHANNEL,
+        LEDC_FADE_NO_WAIT
+    ));
     return 0;
 }
